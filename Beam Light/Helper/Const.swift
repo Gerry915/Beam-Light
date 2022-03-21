9ce92585dbbe1138dd39e2a2de50f3dd6da20620
Gerry915 <gao88915@gmail.com>
2022-03-21 23:37:07 +1100

    Add test target

---
+++ Beam Light/Helper/Const.swift
@@ -0,0 +1,21 @@
+//
+//  Const.swift
+//  Beam Light
+//
+//  Created by Gerry Gao on 16/2/2022.
+//
+
+import Foundation
+
+enum CONSTANT {
+    
+    enum ENDPOINT {
+        static var iTunesURLString: String {
+            return "itunes.apple.com"
+        }
+        static var qod: String {
+            return "https://api.quotable.io/random"
+        }
+    }
+    
+}
