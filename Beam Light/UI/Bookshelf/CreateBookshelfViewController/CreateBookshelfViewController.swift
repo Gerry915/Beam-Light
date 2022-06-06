e458c6e31f20f75a5c7efbd75f07cdaf7e71be7f
Gerry915 <gao88915@gmail.com>
2022-06-07 06:14:24 +1000

    Add blur view to create bookshelf view controller

--- Beam Light/UI/Bookshelf/CreateBookshelfViewController/CreateBookshelfViewController.swift
+++ Beam Light/UI/Bookshelf/CreateBookshelfViewController/CreateBookshelfViewController.swift
@@ -14,8 +14,9 @@ class CreateBookshelfViewController: UIViewController, UITextFieldDelegate {
     let headingLabel: UILabel = {
         let label = UILabel()
         
-        label.text = "What would you like to name it?"
-		label.font = .systemFont(ofSize: UIFont.labelFontSize, weight: .regular)
+        label.text = "What would you like to name your bookshelf?"
+		label.font = .systemFont(ofSize: 24, weight: .bold)
+		label.numberOfLines = 0
         
         return label
     }()
@@ -45,43 +46,56 @@ class CreateBookshelfViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-		view.backgroundColor = .systemBackground
-        
-        inputTextView = PaddingTextField()
-        inputTextView.delegate = self
-        inputTextView.clearButtonMode = .always
-        inputTextView.font = .systemFont(ofSize: 18, weight: .regular)
-		inputTextView.backgroundColor = .secondarySystemBackground
-        inputTextView.layer.cornerRadius = 5
-        
-        inputTextView.placeholder = "Bookshelf name"
-        
-        let buttonContainer = UIView()
-        buttonContainer.addSubview(createButton)
-        
-        let stackView = UIStackView(arrangedSubviews: [headingLabel, inputTextView, buttonContainer])
-        stackView.axis = .vertical
-        stackView.spacing = 20
-        stackView.setCustomSpacing(60, after: inputTextView)
-        
-        view.addSubview(stackView)
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            stackView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -40),
-        ])
-        
-        NSLayoutConstraint.activate([
-            createButton.centerXAnchor.constraint(equalTo: buttonContainer.centerXAnchor),
-            createButton.heightAnchor.constraint(equalTo: buttonContainer.heightAnchor),
-            createButton.widthAnchor.constraint(equalTo: buttonContainer.widthAnchor, multiplier: 0.3)
-        ])
-        
-        createButton.addTarget(self, action: #selector(createBookshelf), for: .touchUpInside)
+
+		setup()
+
     }
+	
+	private func setup() {
+//		view.backgroundColor = .systemBackground
+		setupBlurLayer()
+		
+		inputTextView = PaddingTextField()
+		inputTextView.delegate = self
+		inputTextView.clearButtonMode = .always
+		inputTextView.font = .systemFont(ofSize: 18, weight: .regular)
+		inputTextView.backgroundColor = .secondarySystemBackground
+		inputTextView.layer.cornerRadius = 5
+		
+		inputTextView.placeholder = "Bookshelf name"
+		
+		let buttonContainer = UIView()
+		buttonContainer.addSubview(createButton)
+		
+		let stackView = UIStackView(arrangedSubviews: [headingLabel, inputTextView, buttonContainer])
+		stackView.axis = .vertical
+		stackView.spacing = 20
+		stackView.setCustomSpacing(60, after: inputTextView)
+		
+		view.addSubview(stackView)
+		stackView.translatesAutoresizingMaskIntoConstraints = false
+		
+		NSLayoutConstraint.activate([
+			stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+			stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+			stackView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -40),
+		])
+		
+		NSLayoutConstraint.activate([
+			createButton.centerXAnchor.constraint(equalTo: buttonContainer.centerXAnchor),
+			createButton.heightAnchor.constraint(equalTo: buttonContainer.heightAnchor),
+			createButton.widthAnchor.constraint(equalTo: buttonContainer.widthAnchor, multiplier: 0.3)
+		])
+		
+		createButton.addTarget(self, action: #selector(createBookshelf), for: .touchUpInside)
+	}
+	
+	private func setupBlurLayer() {
+		let blurEffect = UIBlurEffect(style: .prominent)
+		let effectView = UIVisualEffectView(effect: blurEffect)
+		view.addSubview(effectView)
+		effectView.frame = view.frame
+	}
     
     func textFieldDidChangeSelection(_ textField: UITextField) {
         bookshelfName = textField.text
